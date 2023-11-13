d339666637f2f11bfced41ef6cb88ce9dbb634ad
sagruvi <nedelkoaroslav@gmail.com>
2023-11-13 16:08:26 +0000

    add main

---
+++ mymath.go
@@ -0,0 +1,100 @@
+package mymath
+
+import "math"
+
+func Sqrt(x float64) float64 {
+	return math.Sqrt(x)
+}
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+func Pow(x, y float64) float64 {
+	return math.Pow(x, y)
+}
+func Sin(x float64) float64 {
+	return math.Sin(x)
+}
+func Cos(x float64) float64 {
+	return math.Cos(x)
+}
+func Log(x float64) float64 {
+	return math.Log(x)
+}
+func Log10(x float64) float64 {
+	return math.Log10(x)
+}
+func Exp(x float64) float64 {
+	return math.Exp(x)
+}
+func Sinh(x float64) float64 {
+	return math.Sinh(x)
+}
+func Cosh(x float64) float64 {
+	return math.Cosh(x)
+}
+func Tanh(x float64) float64 {
+	return math.Tanh(x)
+}
+func Asin(x float64) float64 {
+	return math.Asin(x)
+}
+func Acos(x float64) float64 {
+	return math.Acos(x)
+}
+func Atan(x float64) float64 {
+	return math.Atan(x)
+}
+func Atan2(x, y float64) float64 {
+	return math.Atan2(x, y)
+}
+func Floor(x float64) float64 {
+	return math.Floor(x)
+}
+func Ceil(x float64) float64 {
+	return math.Ceil(x)
+}
+func Round(x float64) float64 {
+	return math.Round(x)
+}
+func Trunc(x float64) float64 {
+	return math.Trunc(x)
+}
+func Mod(x, y float64) float64 {
+	return math.Mod(x, y)
+}
+func Max(x, y float64) float64 {
+	return math.Max(x, y)
+}
+func Min(x, y float64) float64 {
+	return math.Min(x, y)
+}
+func Pow10(x int) float64 {
+	return math.Pow10(x)
+}
+func Erf(x float64) float64 {
+	return math.Erf(x)
+}
+func Erfc(x float64) float64 {
+	return math.Erfc(x)
+}
+func Lgamma(x float64) (float64, int) {
+	return math.Lgamma(x)
+}
+func J0(x float64) float64 {
+	return math.J0(x)
+}
+func J1(x float64) float64 {
+	return math.J1(x)
+}
+func Y0(x float64) float64 {
+	return math.Y0(x)
+}
+func Y1(x float64) float64 {
+	return math.Y1(x)
+}
+func Gamma(x float64) float64 {
+	return math.Gamma(x)
+}
+func Erfcinv(x float64) float64 {
+	return math.Erfcinv(x)
+}
